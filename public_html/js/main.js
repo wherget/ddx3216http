3c92f0b8de910eb113e9a7f04614b8da5cf5cb27
Wolfgang Herget <r007+github@r007.de>
2015-04-11 15:09:32 +0200

    client main.js: remove code for aux switching (now unused)

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,27 +1,7 @@
 $("input.fader").slider();
 $("input.fader").slider('setValue', -80);
 
-var auxInput = $("#aux");
 var sockIO = io();
-
-$("#next").on("click",function() {
-    var inputValue = auxInput.val();
-    if (inputValue < 4) {
-        auxInput.val(++inputValue);
-    }
-    $("input.fader").each(function (index, el) {
-        $(el).slider('setValue', -80);
-    });
-    $(".channel-strip").data("parameter", auxInput.val());
-});
-
-$("#prev").on("click",function() {
-    var inputValue = auxInput.val();
-    if (inputValue > 1) {
-        auxInput.val(--inputValue);
-    }
-    $(".channel-strip").data("parameter", auxInput.val());
-});
 
 $("input.fader").on("slideStop", function(event) {
     var slider = $(event.currentTarget);
